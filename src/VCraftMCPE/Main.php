b480ff6eba7b0f69b95a7d0618241b3fea6164ae
VCraftMCPE <vcraftmcpe223@gmail.com>
2017-12-28 02:05:39 +0200

    Create Main.php

---
+++ src/VCraftMCPE/Main.php
@@ -0,0 +1,18 @@
+<?php
+namespace VCraftMCPE;
+
+use pocketmine\{Player, Server};
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\utils\{TextFormat};
+
+use pocketmine\item\Item;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\event\Listener;
+use pocketmine\command\{Command,CommandSender, CommandExecutor, ConsoleCommandSender};
+use pocketmine\event\player;
+use jojoe77777\FormAPI;
+use onebone\economyapi\EconomyAPI;
+class Main extends PluginBase implements Listener{
+//tomorrow gn
+}
