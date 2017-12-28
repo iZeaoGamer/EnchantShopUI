888f66fb305490e8f73d44088ac857dc8d1dfb0d
VCraftMCPE <vcraftmcpe223@gmail.com>
2017-12-28 17:02:38 +0200

    Update Main.php

--- src/VCraftMCPE/Main.php
+++ src/VCraftMCPE/Main.php
@@ -13,6 +13,59 @@ use pocketmine\command\{Command,CommandSender, CommandExecutor, ConsoleCommandSe
 use pocketmine\event\player;
 use jojoe77777\FormAPI;
 use onebone\economyapi\EconomyAPI;
+
 class Main extends PluginBase implements Listener{
-//tomorrow gn
+  
+  public function onEnable(){
+        $this->getLogger()->info("Enchant GUI has been  enabled");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+   public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
+        $player = $sender->getPlayer();
+     $cd = strtolower($cmd->getName());
+        switch ($cd){
+            case "enchantshop":
+                $this->EnchantForm($player);
+                break;
+        }
+        return true;
+    }
+  public function EnchantForm(){
+    $plugin = $this->getServer()->getPluginManager();
+        $formapi = $plugin->getPlugin("FormAPI");
+        $form = $formapi->createSimpleForm(function (Player $event, array $args){
+            $result = $args[0];
+            $player = $event->getPlayer();
+            if($result === null){
+            }
+            switch($result){
+                case 0:
+                    return;
+                case 1:
+                    return;
+               case 2:
+                    return;
+                case 3:
+                    return;
+                case 4:
+                    return;
+                case 5:
+                    return;
+                case 6:
+                    return;
+            }
+        });
+     $player = $event->getPlayer();
+    $form->setTitle("enchant");
+        $form->setContent("sdasdasdsa ");
+        $form->addButton("Exit");
+        $form->addButton("");
+        $form->addButton("");
+        $form->addButton("");
+        $form->addButton("");
+        $form->addButton("");
+        $form->addButton("");
+        $form->sendToPlayer($player);
+  }
+  
 }
