377e34bbfb112aef11f567f1c3de540623c9d6d2
VCraftMCPE <vcraftmcpe223@gmail.com>
2017-12-28 17:18:02 +0200

    Update Main.php

--- src/VCraftMCPE/Main.php
+++ src/VCraftMCPE/Main.php
@@ -10,7 +10,6 @@ use pocketmine\item\Item;
 use pocketmine\item\enchantment\Enchantment;
 use pocketmine\event\Listener;
 use pocketmine\command\{Command,CommandSender, CommandExecutor, ConsoleCommandSender};
-use pocketmine\event\player;
 use jojoe77777\FormAPI;
 use onebone\economyapi\EconomyAPI;
 
